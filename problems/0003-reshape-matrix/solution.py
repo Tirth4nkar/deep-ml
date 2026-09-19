48385a7d10231b3b0e82c9aef673098544fcb90f
Tirth4nkar <59279379+Tirth4nkar@users.noreply.github.com>
2026-09-19 07:32:46 +0000

    Update: Reshape Matrix

--- problems/0003-reshape-matrix/solution.py
+++ problems/0003-reshape-matrix/solution.py
@@ -1,9 +1,10 @@
 def reshape_matrix(a: list[list[int | float]], new_shape: tuple[int, int]) -> list[list[int | float]]:
-    target_row, target_cols = new_shape
-    
+    # Write your code here and return a python list after reshaping by using numpy's tolist() method
+	
+	target_row, target_cols = new_shape
     # Flatten original matrix
     flattened = [val for row in a for val in row]
-    
+
     # Check if total elements match the target shape
     if len(flattened) != target_row * target_cols:
         return []
